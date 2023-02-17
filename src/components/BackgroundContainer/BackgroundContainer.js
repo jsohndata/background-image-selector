a794960ee4975c8fda5f69b3d10042d665b2d135
jSohnData <jihos@me.com>
2023-02-17 12:42:21 -0500

    ADDED NEW swtich
    added new feature for targeting specific image

--- src/components/BackgroundContainer/BackgroundContainer.js
+++ src/components/BackgroundContainer/BackgroundContainer.js
@@ -1,13 +1,50 @@
+import { useState } from 'react';
+import { bgList } from '../../data/bgList.js'
 import "./backgroundContainer.css"
 
-export default function BackgroundContainer( {currentBg, handleRandomBG}) {
+export default function BackgroundContainer() {
+    const[currentBg, setCurrentBg] = useState(bgList[0].image);    
+
+    function handleBG( title ) {
+        switch(title) {
+            case 'photoA':
+                return setCurrentBg(bgList[0].image)
+            
+            case 'photoB':
+                return setCurrentBg(bgList[1].image)
+
+            case 'photoC':
+                return setCurrentBg(bgList[2].image)
+
+            case 'photoD':
+                return setCurrentBg(bgList[3].image)
+
+            case 'photoE':
+                return setCurrentBg(bgList[4].image)
+
+            case 'photoF':
+                return setCurrentBg(bgList[5].image)                
+          
+            default:
+                return setCurrentBg(bgList[0].image)
+        }
+    }
 
     return (
-        <div style={{"backgroundImage": `url(/images/${currentBg}`}} 
+        <div style={{"backgroundImage": `url(/images/${currentBg}`}}
              className="bg-container">
                 <div className="bg-box">
-                    <button onClick={ handleRandomBG }>Button</button>
-                    <p><small><a href="https://github.com/jsohndata/random-background-image.git" target="_blank" rel="noreferrer">See Code in GitHub</a></small></p>
+                    <button onClick={ () => handleBG("photoA") }>Photo A</button>
+                    <button onClick={ () => handleBG("photoB") }>Photo B</button>
+                    <button onClick={ () => handleBG("photoC") }>Photo C</button>
+                    <button onClick={ () => handleBG("photoD") }>Photo D</button>
+                    <button onClick={ () => handleBG("photoE") }>Photo E</button>
+                    <button onClick={ () => handleBG("photoF") }>Photo F</button>
+
+                    <p><small><a 
+                        href="https://github.com/jsohndata/random-background-image.git" 
+                        target="_blank" 
+                        rel="noreferrer">See Code in GitHub</a></small></p>
                 </div>
         </div>
     )
--- src/data/bgList.js
+++ src/data/bgList.js
@@ -1,11 +1,45 @@
 export const bgList = [
-    "8408270907_82751777ef_o.webp",
-    "9293836049_456ab06ee3_o.webp",
-    "9293836133_81b8af6eee_o.webp",
-    "9296613184_a418f08ef1_o.webp",
-    "9949629564_42607208c7_o.webp",
-    "16092091780_987656f010_o.webp",
-    "16093335489_7c607735be_o.webp"
+    {
+     id: 1,
+     title: "photoA",
+     image: "8408270907_82751777ef_o.webp",
+    },
+
+    {
+     id: 2,
+     title: "photoB",
+     image: "9293836049_456ab06ee3_o.webp",
+    },
+
+    {
+     id: 3,
+     title: "photoC",
+     image: "9293836133_81b8af6eee_o.webp",
+    },
+
+    {
+     id: 4,
+     title: "photoD",
+     image: "9296613184_a418f08ef1_o.webp",
+    },
+
+    {
+     id: 5,
+     title: "photoE",
+     image: "9949629564_42607208c7_o.webp",
+    },
+
+    {
+     id: 6,
+     title: "photoF",
+     image: "16092091780_987656f010_o.webp",
+    },
+
+    {
+     id: 7,
+     title: "photoG",
+     image: "16093335489_7c607735be_o.webp",
+    },    
 ]
 
 // https://picsum.photos/1400/800?random=1
